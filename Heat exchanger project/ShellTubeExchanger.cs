74c975cc7f2b920d85604229bec89673da3bfc2c
Rustam <ahmetov.rustam2011@gmail.com>
2021-05-19 21:17:06 +0500

    19.05.2021 - 1

---
+++ Heat exchanger project/ShellTubeExchanger.cs
@@ -0,0 +1,11 @@
+﻿namespace Heat_exchanger_project
+{
+    public class ShellTubeExchanger : Exchanger
+    {
+            public ShellTubeExchanger(ColdLiquid coldLiquid, HotLiquid hotLiquid)
+                :base(coldLiquid, hotLiquid)
+            {
+
+            }
+    }
+}
